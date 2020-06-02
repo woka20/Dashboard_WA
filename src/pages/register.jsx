6dd4fa8f928f5626c5c82996637b2c40e4ea24ca
Woka Aditama <woka@Wokas-MacBook-Air.local>
2020-06-02 11:25:07 +0700

    connect with BE using axios

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import {store,actions} from '../store'
 import {withRouter} from 'react-router-dom'
 import {connect} from 'unistore/react'
@@ -11,6 +12,25 @@ import {Container,
 
 
 class RegisterForm extends React.Component{
+    Register=async()=>{
+        req={method:"post",
+             url:"http://127.0.0.1:5000/register",
+             headers:{"Access-Control-Allow-Origin":"*"},
+             data:{name:this.props.company_name,
+                   username:this.props.username,
+                   password:this.props.password
+                }
+            }
+        await axios(req)
+        .then((response)=>{
+            alert(response.data)
+            this.props.history.push("/login")
+
+        })
+        .catch((error)=>{
+            alert(error)
+        })
+    }
     render(){
         return(
             <React.Fragment>
@@ -18,17 +38,17 @@ class RegisterForm extends React.Component{
                     <Form>
                         <FormGroup>
                             <Form.Label>Company Name</Form.Label>
-                            <Form.Control type="text" name="company_name"></Form.Control>
+                            <Form.Control type="text" name="company_name" onChange={event=>this.prop.handleSetGlobal(event)}></Form.Control>
                         </FormGroup>
                         <FormGroup>
                             <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" name="username"></Form.Control>
+                            <Form.Control type="text" name="username" onChange={event=>this.prop.handleSetGlobal(event)}></Form.Control>
                         </FormGroup>
                         <FormGroup>
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="text" name="password"></Form.Control>
+                            <Form.Control type="password" name="password" onChange={event=>this.prop.handleSetGlobal(event)}></Form.Control>
                         </FormGroup>
-                        <Button variant="primary">Register</Button>
+                        <Button variant="primary" onClick={()=>this.Register()}>Register</Button>
                     </Form>
                 </Container>
             </React.Fragment>
@@ -38,4 +58,4 @@ class RegisterForm extends React.Component{
 
 }
 
-export default connect('',actions)(withRouter(RegisterForm))
+export default connect('company_name, username,password',actions)(withRouter(RegisterForm))
